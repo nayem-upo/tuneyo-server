0a5641e8c2c58f6ed9fcd829e62922046279a883
unknown <nayemislam0181@gmail.com>
2023-06-11 01:39:44 +0600

    created collection to db for instructors

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-// const app = express();
-// const port = process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
 //midlewares
 app.use(express.json());
